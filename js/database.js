d0d22f0d7e2df5cf7feef1b5fcb7868e43dcf68b
Atlas <antonio.claudino@discente.ufma.br>
2022-07-21 16:54:10 -0300

    ajuste carrinho

--- js/database.js
+++ js/database.js
@@ -54,7 +54,7 @@ const data = [
   {
     id: 6,
     img: "./img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
+    nameItem: "Champion Jacket",
     description:
       "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
     value: 100.99,
@@ -62,7 +62,3 @@ const data = [
     tag: ["Camisetas"],
   },
 ];
-
-
-const carrinho = []
-
